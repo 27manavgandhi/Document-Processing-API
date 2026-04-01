544dd02c59e80c7972ef283c12835604782173ef
Manav Gandhi <27manavgandhi@gmail.com>
2026-04-01 12:19:27 +0530

    Add src/monitoring/bull-board.ts: Bull queue monitoring dashboard setup

---
+++ src/monitoring/bull-board.ts
@@ -0,0 +1,17 @@
+﻿import { createBullBoard } from '@bull-board/api';
+import { BullMQAdapter } from '@bull-board/api/bullMQAdapter';
+import { ExpressAdapter } from '@bull-board/express';
+import { documentQueue } from '../queues/document.queue';
+import { logger } from '../utils/logger.util';
+
+const serverAdapter = new ExpressAdapter();
+serverAdapter.setBasePath('/admin/queues');
+
+createBullBoard({
+  queues: [new BullMQAdapter(documentQueue)],
+  serverAdapter,
+});
+
+logger.info('Bull Board initialized at /admin/queues');
+
+export const bullBoardRouter = serverAdapter.getRouter();
